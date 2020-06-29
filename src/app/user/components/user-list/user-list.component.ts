9143f742e0acfda4bf3b31af57173e6bd341655f
shouvikchatt <shouvik211207@gmail.com>
2020-06-30 01:50:09 +0530

    Mat-radio, progress-spinner added

--- src/app/user/components/user-list/user-list.component.ts
+++ src/app/user/components/user-list/user-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {ThemePalette} from '@angular/material/core';/* Added by Estilo 29/06/2020 */
+import {ProgressSpinnerMode} from '@angular/material/progress-spinner';/* Added by Estilo 29/06/2020 */
 
 import { DataResolverService } from '../../services/data-resolver.service';
 
@@ -11,9 +13,16 @@ import { DataResolverService } from '../../services/data-resolver.service';
 export class UserListComponent implements OnInit {
 
   public employeeList;
+  /* Added by Estilo 29/06/2020 */
+  public isLoader:Boolean = false;
+  public color: ThemePalette = 'accent';
+  public mode: ProgressSpinnerMode = 'indeterminate';
+  public value = 100;
+  public selectedEmp;
+  /*------------------------------ */
 
   constructor(
-    private roter: Router,
+    private router: Router,
     private dataResolverService: DataResolverService) {
 
   }
@@ -24,14 +33,16 @@ export class UserListComponent implements OnInit {
 
   getEmployeeList() {
     console.log('Get Fired!');
-
+    this.isLoader = true; /* Added by Estilo 29/06/2020 */
     this.dataResolverService.getEmployeeList().subscribe(
       response => {
         console.log('xxxx Response', response);
+        this.isLoader = false; /* Added by Estilo 29/06/2020 */
         this.employeeList = response;
       },
       error => {
         console.log('xxxx Error', error);
+        this.isLoader = false; /* Added by Estilo 29/06/2020 */
       });
   }
 
@@ -41,17 +52,20 @@ export class UserListComponent implements OnInit {
     this.dataResolverService.delEmployee(id).subscribe(
       response => {
         console.log('yyyy Response', response);
+        this.isLoader = true; /* Added by Estilo 29/06/2020 */
         this.getEmployeeList();
       },
       error => {
         console.log('yyyy Error', error);
+        this.isLoader = false; /* Added by Estilo 29/06/2020 */
       });
   }
 
   onAdd() {
     console.log('Add Fired!');
 
-    this.roter.navigate(['user','add']);
+    // this.router.navigate(['user','add']);
+    this.router.navigate(['/user/add']); /* Added by Estilo 29/06/2020 */
   }
 
   onEdit() {
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';/* Added by Estilo 29/06/2020 */
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';/* Added by Estilo 29/06/2020 */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';/* Added by Estilo 29/06/2020 */
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
+
 
 @NgModule({
   declarations: [
@@ -16,9 +20,13 @@ import { UserFormComponent } from './components/user-form/user-form.component';
   imports: [
     CommonModule,
     UserRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatIconModule,
     MatButtonModule,
     MatRippleModule,
+    MatRadioModule,
+    MatProgressSpinnerModule
   ]
 })
 export class UserModule { }
